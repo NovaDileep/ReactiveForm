5c9eb5f4134ad3adb00df0a1a3081760dfaaab2c
NovaDileep <nova.cs17@jecc.ac.in>
2021-06-14 18:13:57 +0530

    reactive form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reactiveform';
+  constructor (private fb:FormBuilder){}
+  registerform=this.fb.group({
+    email:['',[Validators.pattern('^[a-z0-9,%+]+@[a-z0-9.-]+\.[a-z]{2,4}$'),Validators.required]],
+    password:['',[Validators.minLength(6),Validators.required]]
+  })
 }
